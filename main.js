706e2eaa83fc557b0cf93e18142bd366c102dae3
Proxtx <gopro54122@gmail.com>
2022-01-25 21:48:43 +0100

    catch block

--- main.js
+++ main.js
@@ -36,72 +36,82 @@ await new Promise((r) => fileLoop(r));
 const dataLoop = async () => {
   while (true) {
     console.log("Data update");
-    let members = await myCircle.listMembers();
-    let date = Date.now();
-    data[date] = {};
+    try {
+      let members = await myCircle.listMembers();
+      let date = Date.now();
+      data[date] = {};
 
-    for (const member of members) {
-      data[date][member.id] = {
-        features: member.features,
-        issues: member.issues,
-        location: {
-          latitude: member.location.latitude,
-          longitude: member.location.longitude,
-          accuracy: member.location.accuracy,
-          startTimestamp: member.location.startTimestamp,
-          endTimestamp: member.location.endTimestamp,
-          since: member.location.since,
-          timestamp: member.location.timestamp,
-          name: member.location.name,
-          placeType: member.location.placeType,
-          source: member.location.source,
-          sourceId: member.location.sourceId,
-          address1: member.location.address1,
-          address2: member.location.address2,
-          shortAddress: member.location.shortAddress,
-          inTransit: member.location.inTransit,
-          tripId: member.location.tripId,
-          deriveSDKStatus: member.location.deriveSDKStatus,
-          battery: member.location.battery,
-          charge: member.location.charge,
-          wifiState: member.location.wifiState,
-          speed: member.location.speed,
-          isDriving: member.location.isDriving,
-          userActivity: member.location.userActivity,
-        },
-        communications: member.communications,
-        medical: member.medical,
-        relation: member.relation,
-        createdAt: member.createdAt,
-        activity: member.activity,
-        id: member.id,
-        firstName: member.firstName,
-        lastName: member.lastName,
-        isAdmin: member.isAdmin,
-        pinNumber: member.pinNumber,
-        loginEmail: member.loginEmail,
-        loginPhone: member.loginPhone,
-      };
+      for (const member of members) {
+        data[date][member.id] = {
+          features: member.features,
+          issues: member.issues,
+          location: {
+            latitude: member.location.latitude,
+            longitude: member.location.longitude,
+            accuracy: member.location.accuracy,
+            startTimestamp: member.location.startTimestamp,
+            endTimestamp: member.location.endTimestamp,
+            since: member.location.since,
+            timestamp: member.location.timestamp,
+            name: member.location.name,
+            placeType: member.location.placeType,
+            source: member.location.source,
+            sourceId: member.location.sourceId,
+            address1: member.location.address1,
+            address2: member.location.address2,
+            shortAddress: member.location.shortAddress,
+            inTransit: member.location.inTransit,
+            tripId: member.location.tripId,
+            deriveSDKStatus: member.location.deriveSDKStatus,
+            battery: member.location.battery,
+            charge: member.location.charge,
+            wifiState: member.location.wifiState,
+            speed: member.location.speed,
+            isDriving: member.location.isDriving,
+            userActivity: member.location.userActivity,
+          },
+          communications: member.communications,
+          medical: member.medical,
+          relation: member.relation,
+          createdAt: member.createdAt,
+          activity: member.activity,
+          id: member.id,
+          firstName: member.firstName,
+          lastName: member.lastName,
+          isAdmin: member.isAdmin,
+          pinNumber: member.pinNumber,
+          loginEmail: member.loginEmail,
+          loginPhone: member.loginPhone,
+        };
+      }
+      await new Promise((r) => setTimeout(r, config.fullDataInterval));
+    } catch (e) {
+      console.log("Data update failed retry in 15s");
+      await new Promise((r) => setTimeout(r, 15000));
     }
-    await new Promise((r) => setTimeout(r, config.fullDataInterval));
   }
 };
 
 const locationLoop = async () => {
   while (true) {
     console.log("Location Update");
-    let members = await myCircle.listMembers();
-    let date = Date.now();
-    locations[date] = {};
+    try {
+      let members = await myCircle.listMembers();
+      let date = Date.now();
+      locations[date] = {};
 
-    for (const member of members) {
-      locations[date][member.id] = {
-        latitude: member.location.latitude,
-        longitude: member.location.longitude,
-        address: member.location.address1,
-      };
+      for (const member of members) {
+        locations[date][member.id] = {
+          latitude: member.location.latitude,
+          longitude: member.location.longitude,
+          address: member.location.address1,
+        };
+      }
+      await new Promise((r) => setTimeout(r, config.locationInterval));
+    } catch (e) {
+      console.log("Location update failed retry in 15s");
+      await new Promise((r) => setTimeout(r, 15000));
     }
-    await new Promise((r) => setTimeout(r, config.locationInterval));
   }
 };
 
